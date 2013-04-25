69d431249ed0d3bb1ddc95da12de12056764b262
Chris Withers <chris@simplistix.co.uk>
2013-04-25 09:22:08 +0100

    refactor tests.

--- mortar_rdb/tests/test_scripts.py
+++ mortar_rdb/tests/test_scripts.py
@@ -48,6 +48,14 @@ class TestCreate(ScriptsMixin, PackageTest):
         super(TestCreate, self).setUp()
         self.db_url = 'sqlite:///'+self.dir.getpath('sqlite.db')
 
+    def _setup_config(self):
+        # setup
+        metadata = MetaData()
+        self.mytable = Table('user', metadata,
+                             Column('id', Integer, primary_key=True),
+                             )
+        self.config = Config(Source(self.mytable))
+        
     def _check_db(self, expected_metadata):
         actual_metadata = MetaData(bind=create_engine(self.db_url))
         actual_metadata.reflect()
@@ -56,17 +64,10 @@ class TestCreate(ScriptsMixin, PackageTest):
                 actual_metadata.tables.keys())
             
     def test_url_from_command_line(self):
-        # setup
-        metadata = MetaData()
-        mytable = Table('user', metadata,
-                        Column('id', Integer, primary_key=True),
-                        )
-        self.config = Config(Source(mytable))
-
-        db_url = self.db_url
+        self._setup_config()        
         # make sure we're actually using the url from the command line:
+        db_url = self.db_url
         self.db_url = 'junk'
-            
         self._check('--url=%s create' % db_url, '''
 For database at %s:
 
@@ -75,24 +76,16 @@ user
 ''' % (db_url, ))
             
     def test_single_source(self):
-        # setup
-        metadata = MetaData()
-        mytable = Table('user', metadata,
-                        Column('id', Integer, primary_key=True),
-                        )
-        self.config = Config(Source(mytable))
-
-        # check 
-        
+        self._setup_config()
+        # check         
         self._check('create','''
 For database at %s:
 
 Creating the following tables:
 user
 ''' % (self.db_url, ))
-
         expected_metadata = MetaData()
-        mytable.tometadata(expected_metadata)
+        self.mytable.tometadata(expected_metadata)
         self._check_db(expected_metadata)
 
     def test_multi_source(self):
@@ -126,25 +119,17 @@ t2
         self._check_db(expected_metadata)
     
     def test_table_present(self):
-        # setup
-        metadata = MetaData()
-        mytable = Table('user', metadata,
-                        Column('id', Integer, primary_key=True),
-                        )
-        mytable.create(create_engine(self.db_url))
-        self.config = Config(Source(mytable))
-
-        # check 
-        
+        self._setup_config()
+        self.mytable.create(create_engine(self.db_url))
+        # check         
         self._check('create','''
 For database at %s:
 
 Refusing to create as the following tables exist:
 user
 ''' % self.db_url)
-
         expected_metadata = MetaData()
-        mytable.tometadata(expected_metadata)
+        self.mytable.tometadata(expected_metadata)
 
 
 class TestDrop(ScriptsMixin, ControlledTest):
